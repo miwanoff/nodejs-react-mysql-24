8ce1d5fd4072c2a40909f48e9f0572fea9359ef8
miwanoff <marina.iva.noffa@gmail.com>
2024-06-09 12:32:17 +0300

    Add login button

--- client/src/routers/Home.jsx
+++ client/src/routers/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 const Home = () => {
   return (
-    <div className="title">
+    <div className="title text-center">
       <h1 className="text-center mt-5 mb-3">Node School</h1>
+      <a href="/Login" className="btn btn-warning my-3">
+        Link Button
+      </a>
     </div>
   );
 };
-
 
 export default Home;
